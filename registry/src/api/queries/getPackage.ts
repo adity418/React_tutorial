d37e071495db82431dec2d4305931a77422037ba
aditya-dev <adity119@gmail.com>
2025-03-13 17:09:58 +0530

    completed the project

--- registry/src/api/queries/getPackage.ts
+++ registry/src/api/queries/getPackage.ts
@@ -1,7 +1,7 @@
 import type { PackageDetails } from "../types/packageDetails";
 
 export async function getPackage(name: string) : Promise<PackageDetails> {
-    const res = await fetch(`https://registery.npmjs.org/${name}`);
+    const res = await fetch(`https://registry.npmjs.org/${name}`);
     const data = await res.json();
 
     return data as PackageDetails;
