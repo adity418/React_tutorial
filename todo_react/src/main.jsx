8c2c41bf07d9cbd78f317d43f923860347c4b50b
aditya-dev <adity119@gmail.com>
2025-03-27 13:01:01 +0530

    Update styles for App.css and main.jsx

--- todo_react/src/main.jsx
+++ todo_react/src/main.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import './index.css';
+import Todo from "./App";
 
 const el = document.getElementById("root");
 const root = ReactDOM.createRoot(el)
 
-root.render(<App />)
+root.render(<Todo />)
